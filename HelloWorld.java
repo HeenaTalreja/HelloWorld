046383f92b46440605882034d024bbef300ac328
heenaTalreja <heena.talreja@einfochips.com>
2018-07-23 17:59:32 +0530

    added maven

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
 		 System.out.println("Trigger Build"); // Display the string.
+		  System.out.println("Added Maven"); // Display the string.
+		   System.out.println("Added Maven"); // Display the string.
     }
 }
